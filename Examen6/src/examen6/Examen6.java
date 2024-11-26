c21b27889f6aae6d892af13fc06da057f8c1e686
Javier Antonio Guzmán Patiño <s23120251@alumnos.itsur.edu.mx>
2024-11-26 08:39:38 -0600

    Update Examen6.java

--- Examen6/src/examen6/Examen6.java
+++ Examen6/src/examen6/Examen6.java
@@ -15,7 +15,7 @@ public class Examen6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        System.out.println("Hola mundo");
     }
     
 }
